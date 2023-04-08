97e0e8c16e3f3708cc2ad19b55113bc448a7c258
Devangi Patel <devangipatel7601@gmail.com>
2023-04-08 08:33:59 +0100

    commit1

--- src/LoopMultiplicationTable.java
+++ src/LoopMultiplicationTable.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
 
 public class LoopMultiplicationTable {
-    //creating scanner class object
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        //acknowledge user to input
         System.out.println("Enter any Number: ");
-
-        //create variable using scanner object
         int a = scanner.nextInt();
 
         for (int i=1; i<=12; i++ ){
-
-           //for printing multiplication
-            { System.out.println( a+" X "+ i +" = "+ (i*a));
+           // System.out.println(i);
+            //for (int j=1; j<=12; j++){
+            { System.out.println( a+"X"+i+"="+(i*a));
             }
         }
     }
